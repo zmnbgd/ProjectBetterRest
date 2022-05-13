4cf03313445f8947e06fe4b1e4bee9e6d53a81f9
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2022-05-14 00:10:10 +0200

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Marko Zivanovic on 10.5.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeAmount = 1
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up")
+                    .font(.headline)
+                
+                DatePicker("Plese enter the time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desire amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeAmount == 1 ? "1 cup" : "\(coffeAmount) cups", value: $coffeAmount, in: 1...10)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+        }
+    }
+    
+    func calculateBedtime() {
+        
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
